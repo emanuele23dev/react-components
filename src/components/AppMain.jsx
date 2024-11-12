a79aad4a656bf5474603b5b67429ddc8f71e865b
emanuele23dev <emanuele.meggiarin@libero.it>
2024-11-12 17:18:52 +0100

    add post blog

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,3 +1,5 @@
+import crashImg from "../assets/img/crash.png";
+
 export default function AppMain() {
   const title = "Il mio Blog React";
 
@@ -6,7 +8,21 @@ export default function AppMain() {
       <div className="container">
         <h2 className="blog-title">{title}</h2>
 
-        <section className="blog-post"></section>
+        <section className="blog-post">
+          <div className="blog-card">
+            <img src={crashImg} alt="" />
+            <div className="blog-title-card">
+              <h3>Crash Team Racing</h3>
+              <p>
+                Crash Team Racing, pubblicato come Crash Bandicoot Racing in
+                Giappone, è un videogioco di guida sviluppato da Naughty Dog nel
+                1999 tratto dalla serie di videogiochi a piattaforme Crash
+                Bandicoot.
+              </p>
+            </div>
+            <button className="play">Play</button>
+          </div>
+        </section>
       </div>
     </main>
   );
